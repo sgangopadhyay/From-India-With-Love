b269174ea865eff4b5a12345c9cacdaac0fcb5ce
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-10-21 20:46:58 +0530

    Create add_multiply.py

---
+++ add_multiply.py
@@ -0,0 +1,14 @@
+number = 56
+sum=0
+for i in str(number):
+    sum = sum + int(i) 
+print("Sum of the Digits of ",number," is ", sum)
+
+
+
+# Write a program to find out the multiplication of all its integers ?
+sum=0
+multiply_1 =1
+for i in str(number):
+    multiply_1 = multiply_1 * int(i) 
+print("Multiplication of the Digits of ",number," is ", multiply_1)
