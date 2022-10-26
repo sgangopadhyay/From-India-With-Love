dfd2ea3310807ec02ef13eae394987e0e932921c
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-10-26 20:18:06 +0530

    Create divide_list_half.py

---
+++ divide_list_half.py
@@ -0,0 +1,15 @@
+# You have been given a LIST create two lists from the existing lists 
+
+
+list_1 = [10,11,12,13,10,13,15,17,19,17,19]
+
+print(list_1)
+
+half_length_list = len(list_1)//2
+
+print(half_length_list)
+
+print(list_1[:half_length_list]) # 0 to 4
+
+print(list_1[half_length_list:]) # 5 to 10
+
